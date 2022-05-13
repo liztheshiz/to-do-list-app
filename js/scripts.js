648c0f8a7bd69dfca796132ad09de532c66cf545
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-05-13 09:49:01 -0400

    rewrote original js in jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,38 @@ li.on('click', function() {
 });
 */
 
+function newItem() {
+	// 1. Add a new item to the list of items
+	let li = $('<li></li>');
+	let inputValue = $('#input').val();
+	li.append(inputValue);
+
+	if (inputValue === '') {
+		alert('You must write something!');
+	} else {
+		let list = $('#list');
+		list.append(li);
+	}
+
+	// 2. Cross out an item from the list of items
+	function crossOut() {
+		li.addClass('strike');
+	}
+
+	li.on('dblclick', crossOut);
+
+	// 3(i). Add the delete button 'X'
+	let crossOutButton = $('<crossOutButton></crossOutButton>');
+	crossOutButton.append(document.createTextNode('X'));
+	li.append(crossOutButton);
+
+	// 3(ii). Add class .delete (display: none) from the css
+	function deleteListItem() {
+		li.addClass('delete');
+	}
+
+	crossOutButton.on('click', deleteListItem);
+
+	// 4. Reorder the items
+	$('#list').sortable();
+}
