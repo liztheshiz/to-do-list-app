b7e530c241b86e2b89be429a51a331a493daa42b
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-05-13 09:51:33 -0400

    removed some notes from comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,10 @@
 /*
-let list = $('#list');
-let li = $('<li></li>');
-
-list.append(li);
-
-let inputValue = $('#input').val();
-
-li.append('hi');
-
-li.addClass('strike');
-
 $('input').each(function (i) {
 	let inputValue = $(this).val();
 	let inputName = $(this).attr('name');
 	if(inputValue().length < 1){
 	  console.log('Please fill out the ' + inputName + ' field')
 	}
-});
-
-li.on('click', function() {
-    li.addClass('strike');
 });
 */
 
